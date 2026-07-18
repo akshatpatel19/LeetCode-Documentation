f2cc4045c879478dbdad0f84ff8f19a551671064
Akshat Patel <86944851+akshatpatel19@users.noreply.github.com>
2026-07-18 13:10:14 +0530

    Time: 7 ms (89.19%), Space: 46.2 MB (51.08%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        int left = 0;
+        int right = 0;
+        int maxlen = 0;
+        int maxfreq = 0;
+        int hmap[] = new int[26];
+        while(right<s.length()){
+            hmap[s.charAt(right)-'A']++;
+            maxfreq = Math.max(maxfreq,hmap[s.charAt(right)-'A']);
+            int chan = right-left+1-maxfreq;
+            if(chan>k){
+                hmap[s.charAt(left)-'A']--;
+                maxfreq=0;
+                for(int i = 0;i<26;i++){
+                    maxfreq = Math.max(maxfreq,hmap[i]);
+                }
+                left++;
+            }
+            if(chan<=k){
+                maxlen = Math.max(maxlen,right-left+1);
+            }
+            right++;
+        }
+        return maxlen;
+    }
+}
