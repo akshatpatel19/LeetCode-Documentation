6d1064667a420f1d6a255c53da936175370cd505
Akshat Patel <86944851+akshatpatel19@users.noreply.github.com>
2026-07-31 21:22:12 +0530

    Time: 100 ms (5.49%), Space: 48 MB (52.6%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int minimumPushes(String word) {
+        HashMap<Character, Integer> hmap = new HashMap<>();
+        for(int i = 0;i<word.length();i++){
+            int val = hmap.getOrDefault(word.charAt(i),0);
+            hmap.put(word.charAt(i),val+1);
+        }
+        int arr[] = new int[hmap.size()];
+        int i = 0;
+        for(int num : hmap.values()){
+            arr[i] = num;
+            i++;
+        }
+        Arrays.sort(arr);
+        i = 0;
+        int cnt = 0;
+        int sum = 0;
+        for(int j = arr.length-1;j>=0;j--){
+            if(i%8==0){
+                cnt++;
+            }
+            System.out.print(arr[j]+"*"+cnt+" ");
+            sum+=arr[j]*cnt;
+            i++;
+        }
+        return sum;
+    }
+}
